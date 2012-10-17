05455e22e36e1ef6ca80f45c686f8e810c87e93d
Henri Chen <henrichen@zkoss.org>
2012-10-17 12:10:18 +0800

    HelloCordova example

---
+++ example/hellocordova/HelloCordova.dart
@@ -0,0 +1,38 @@
+//import 'package:rikulo/view.dart';
+import '../../../rikulo/lib/view.dart';
+import 'package:rikulo_gap/gap.dart';
+
+showAcceleration() {
+  //prepare a text view to show the acceleration information
+  TextView welcome = new TextView("Hello Accelerometer!");
+  welcome.profile.text = "anchor:  parent; location: center center";
+  new View()..addToDocument()
+            ..addChild(welcome);
+
+  //watch accelerometer and show the acceleration in x, y and z axis.
+  accelerometer.watchAcceleration(
+    (accel) {
+      welcome.text = "${accel.timestamp},\n"
+                   "x:${accel.x},\n"
+                   "y:${accel.y},\n"
+                   "z:${accel.z}";
+      welcome.requestLayout(true); },
+    () => print("Fail to get acceleration"),
+    new AccelerometerOptions(frequency:1000)
+  );
+}
+
+void main() {
+  //enable the device
+  Future<Device> enable = enableDeviceAccess();
+
+  //when device is enabled and ready
+  enable.then((device) => showAcceleration());
+
+  //if failed to enable the device and/or timeout!
+  enable.handleException((ex) {
+    print("Fail to enable the device.");
+    return true;
+  });
+}
+
